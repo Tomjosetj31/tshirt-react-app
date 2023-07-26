14d2c63271f6bde7cb3a356110c02ed6ed17ab69
Tomjosetj31 <tomjosethomastj31@gmail.com>
2023-07-26 22:03:03 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ARScene from "./ARScene";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your other components and UI elements can go here */}
+      <ARScene />
+    </div>
+  );
+}
+
+export default App;
